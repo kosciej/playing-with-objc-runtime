8471668eb9ce4d40b522969dd6420133b52e8b3b
Michał Zabielski <zabielski.michal@gmail.com>
2017-04-13 20:15:00 +0200

    Initial Commit

---
+++ Playing with objc runtime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Playing with objc runtime
+//
+//  Created by Michal Zabielski on 13.04.2017.
+//  Copyright © 2017 Kosciej. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Playing with objc runtime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Playing with objc runtime
+//
+//  Created by Michal Zabielski on 13.04.2017.
+//  Copyright © 2017 Kosciej. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
